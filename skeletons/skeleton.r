3c07624d0c29b32dec3a703ee8666625cdb96024
Jongbin Jung <jongbin@stanford.edu>
2016-10-26 11:10:55 -0700

    Add skeleton file for R

---
+++ skeletons/skeleton.r
@@ -0,0 +1,10 @@
+#!/usr/bin/r
+# Created: `date +%F`
+# Author: ${1:`id -u -n`}
+
+library(${2:tidyverse})
+
+theme_set(theme_bw)
+
+# Start main script ############################################################
+${0}
