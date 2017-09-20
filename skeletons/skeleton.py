4728655455354200fb2b1ddac4855aadfbafb960
Jongbin Jung <jongbin@stanford.edu>
2017-09-20 15:42:57 -0700

    Use python3 by default

--- skeletons/skeleton.py
+++ skeletons/skeleton.py
@@ -1,19 +1,17 @@
-#!/usr/bin/env python${1:2}
-'''
+#!/usr/bin/env python${1:3}
+"""
 ${2:New python module}
 
 Created: `date +%F`
 Author: ${3:`id -u -n`}
-'''
+"""
 
 import ${4:package_name}
 
 
 def main():
-    '''
-    ${5:Main function}
-    '''
+    """${5:Main function}"""
     ${0:# Write code here}
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
